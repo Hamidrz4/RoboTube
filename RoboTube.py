d82622ee8b87900f48f289d4e174952856b5907a
Hamidrz4 <110513835+Hamidrz4@users.noreply.github.com>
2023-08-27 20:31:46 +0330

    Update RoboTube.py

--- RoboTube.py
+++ RoboTube.py
@@ -1,4 +1,4 @@
-from pytube import Playlist, YouTube
+from pytube import Playlist, YouTube,exceptions
 
 def load_paths_from_file():
     paths = []
@@ -39,13 +39,23 @@ def select_path(paths):
 
 def download_video(video_link, sp):
     vid = YouTube(video_link)
-    st = vid.streams.get_by_itag(22)
-    if st is not None:
-        print("Downloading:", vid.title)
-        st.download(sp)
-        print(f"Downloaded {vid.title}")
-    else:
-        print(f"No stream available for {vid.title}")
+    try:
+        st = vid.streams.get_by_itag(22)  # Update the itag based on the desired stream
+        if st is not None:
+            print("Video:", vid.title)
+            print("File size:", st.filesize / (1024 * 1024), "MB")
+            download_choice = input("Do you want to download this video? (yes/no): ")
+            if download_choice.lower() == "yes":
+                print("Downloading...")
+                st.download(output_path=sp)
+                print(f"Downloaded {vid.title}")
+            else:
+                print("Download canceled.")
+        else:
+            print(f"No stream available for {vid.title}")
+    except exceptions.AgeRestrictedError:
+        print("The video is age restricted and can't be downloaded without logging in.")
+
 
 def download_playlist(playlist_link, quality, sp):
     p = Playlist(playlist_link)
@@ -57,18 +67,50 @@ def download_playlist(playlist_link, quality, sp):
             print("Downloading:", video.title)
             st.download(sp)
             print(f"Downloaded {video.title} in {quality}.")
+
+            if video.captions:
+                download_caption(video, sp)
         else:
             print(f"No {quality} stream available for {video.title}.")
+            n += 1
 
 def download_audio(audio_link, sp):
     audio = YouTube(audio_link)
     st = audio.streams.get_by_itag(251)
     if st is not None:
-        print("Downloading:", audio.title)
-        st.download(sp)
-        print(f"Downloaded {audio.title}")
+        print("Audio:", audio.title)
+        print("File size:", st.filesize / (1024 * 1024), "MB")  # Convert bytes to MB
+        download_choice = input("Do you want to download this audio? (yes/no): ")
+        if download_choice.lower() == "yes":
+            print("Downloading...")
+            st.download(output_path=sp)
+            print(f"Downloaded {audio.title}")
+        else:
+            print("Download canceled.")
     else:
         print(f"No stream available for {audio.title}")
+
+def download_caption(video, sp):
+    print("Available caption tracks:")
+    for caption in video.captions:
+        print(f"{caption.code}: {caption.name}")
+
+    caption_code = input("Enter the caption track code to download: ")
+    selected_caption = None
+
+    for caption in video.captions:
+        if caption_code in caption.code:
+            selected_caption = caption
+            break
+
+    if selected_caption:
+        print(f"Downloading caption track '{selected_caption.name}'...")
+        caption_text = selected_caption.xml_captions
+        with open(f"{sp}/{video.title}_{selected_caption.code}.xml", "w", encoding="utf-8") as caption_file:
+            caption_file.write(caption_text)
+        print(f"Caption track '{selected_caption.name}' downloaded in XML format.")
+    else:
+        print("Invalid caption track code.")
 
 def main():
     paths = load_paths_from_file()
@@ -79,7 +121,7 @@ def main():
     sp = select_path(paths)
 
     while True:
-        user_input = input("Enter 'video' or 'playlist' or 'audio' to continue, or 'exit' to quit: ").lower()
+        user_input = input("Enter 'video', 'playlist', 'audio', 'caption' to continue, or 'exit' to quit: ").lower()
 
         if user_input == 'exit':
             print("Exiting the program.")
@@ -101,11 +143,21 @@ def main():
             audio_link = input("Paste Your Audio Link: ")
             download_audio(audio_link, sp)
 
+        elif user_input == 'caption':
+            print("Caption Mode:")
+            video_link = input("Paste Your Video Link: ")
+            vid = YouTube(video_link)
+            if vid.captions:
+                download_caption(vid, sp)
+            else:
+                print("No captions available for this video.")
+
         else:
-            print("Invalid input. Please enter 'video' or 'playlist' or 'audio' or 'exit'.")
+            print("Invalid input. Please enter 'video', 'playlist', 'audio', 'caption', or 'exit'.")
 
 if __name__ == "__main__":
     main()
+
 
 
 
